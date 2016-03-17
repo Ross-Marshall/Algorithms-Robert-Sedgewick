ae0d629950dc9624cd0dbf33ed912594da65d37b
Ross Marshall <rossmarshallgithub@gmail.com>
2016-03-16 23:45:01 -0400

    11th hour checking

---
+++ Convert/Convert.java
@@ -0,0 +1,11 @@
+public class Convert {
+
+
+    public static void main( String [] args ) {
+
+        String inNum = args[0];
+
+        System.out.println( "Convert: Input [" + inNum + "]" );
+    }
+
+}
