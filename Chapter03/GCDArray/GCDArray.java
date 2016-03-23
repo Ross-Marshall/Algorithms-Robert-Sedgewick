a971e7e9c6cd10c08a38278dd344c9a9f098a559
Ross Marshall <rossmarshallgithub@gmail.com>
2016-03-22 23:06:59 -0400

    Boolean array. If GCD of (i,j) = 1 set to 1, 0 otherwise.

---
+++ Chapter03/GCDArray/GCDArray.java
@@ -0,0 +1,50 @@
+public class GCDArray {
+
+   int gcd( int u, int v ) {
+        int t;
+
+        if ( v == 0 ) return 0;
+
+        while ( u > 1 ) {
+    
+            if ( u < v ) {
+                t = u;
+                u = v;
+                v = t;
+            }       
+
+            u = u - v;
+            
+        }
+        return u;
+    }
+
+    public static void main ( String [] args ) {
+        
+        int max_x = Integer.parseInt( args[0] );
+        int max_y = Integer.parseInt( args[1] );
+
+        int [][] a = new int[ max_x+1 ][ max_y+1 ];
+
+        GCDArray g = new GCDArray();
+
+        for ( int i = 0 ; i < max_x ; i++ ) {
+            for ( int j = 0 ; j < max_y ; j++ ) {
+
+                 if ( g.gcd( i, j ) == 1 ) {
+                     a[i][j] = 1;
+                 }  else {
+                     a[i][j] = 0;
+                 }
+            }
+        } 
+
+        for ( int i = 0 ; i < max_x ; i++ ) {
+            for ( int j = 0 ; j < max_y ; j++ ) {
+                 System.out.print( a[i][j] + " " );
+            }
+            System.out.println("");
+        }    
+         
+    }
+}
