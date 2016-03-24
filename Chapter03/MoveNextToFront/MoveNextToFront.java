d23b333bd7b30ed1d63675bc0edc626c6f078950
Ross Marshall <rossmarshallgithub@gmail.com>
2016-03-23 23:21:28 -0400

    Move Next To Front

---
+++ Chapter03/MoveNextToFront/MoveNextToFront.java
@@ -0,0 +1,54 @@
+public class MoveNextToFront {
+        
+    Node head = new Node( -1 );
+    Node tail = new Node( -2 );
+
+    public void moveNextToFront( int key ) {
+        Node m = this.head;
+        while ( m.getKey() != key-1 ) {
+            m = m.getNext();
+        }
+        System.out.println( "moveNextToFront : "  + m.getKey() );
+   
+        Node n = m.getNext();
+
+        System.out.println( "moveNextToFront : "  + n.getKey() );
+
+        m.setNext( n.getNext() );
+        head.getNext().setNext( head.getNext() );
+        head.setNext( m );
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int MAX = 25;
+        Node t;
+
+        MoveNextToFront m = new MoveNextToFront();
+        m.head.setNext( m.tail );
+        m.tail.setNext( m.tail );
+
+        for (int i = 0; i <= MAX ; i++) {
+            t = new Node( i );
+            t.setNext( m.head.getNext() );
+            m.head.setNext( t );
+        }
+        
+        t = m.head;
+        while (t != t.getNext()) {
+            System.out.println( "Key: " + t.getKey() );
+            t = t.getNext();
+        }
+
+        m.moveNextToFront( 12 );
+        
+        System.out.println( "After moveNextToFront()....." );
+        t = m.head;
+        while (t != t.getNext()) {
+            System.out.println( "Key: " + t.getKey() );
+            t = t.getNext();
+        }
+    }
+}
---
+++ Chapter03/MoveNextToFront/Node.java
@@ -0,0 +1,28 @@
+public class Node {
+    
+      public Node ( int key ) {
+          setKey( key );
+      }
+      
+      public Node getNext() {
+            return next;
+        }
+        
+        public int getKey() {
+            return key;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+        public int key;
+        public Node next;
+        
+        public String toString() {
+            return Integer.toString ( getKey() );
+        }
+        
+        public void setKey( int key ) {
+            this.key = key;
+        }
+}
