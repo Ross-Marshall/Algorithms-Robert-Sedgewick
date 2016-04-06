9800f9e48a5b4dd4d4184b7988c29c6780703e3a
Ross Marshall <rossmarshallgithub@gmail.com>
2016-04-05 23:23:25 -0400

    Start building tree

--- Chapter04/VisitOrder/visit_order.py
+++ Chapter04/VisitOrder/visit_order.py
@@ -9,23 +9,28 @@ from stack import Stack
 class VisitOrder:
   
     stack = None 
+    level = 0
 
     def __init__( self ):
         self.stack = Stack()
 
-    def make_tree( self ):
+    def make_stack( self ):
         while self.stack.length() > 0:
             print "self.stack.length() ==> " + str( self.stack.length() )
-            print "Pop the stack ---> " + str( self.stack.pop().value )
-        #if self.stack.empty() != False:
-        #   node.left = stack.pop()
-        #   build(node.left)
-        #if self.stack.empty() != False:
-        #   node.right = stack.pop()
-        #   build(node.right)
+            #print "Pop the stack ---> " + str( self.stack.pop().value )
 
-    #def main(args):
-   
+
+    def build_tree( self, node ):
+         self.level = self.level + 1
+         print "in build_tree level : " + str( self.level )
+         while len( node.value ) > 0:
+             node.left = self.stack.pop() 
+             build_tree( node.left )
+         
+         while len( node.value ) > 0:
+             node.right = self.stack.pop()
+             build_tree( node.right )
+  
 if __name__ == "__main__":
     #print str( sys.argv[1] )
     print "Visit Order"
@@ -44,5 +49,8 @@ if __name__ == "__main__":
 
     vo.stack.print_stack()
 
-    vo.make_tree ()
+    #vo.make_stack ()
+
+    vo.build_tree( vo.stack.pop() )
+
 
