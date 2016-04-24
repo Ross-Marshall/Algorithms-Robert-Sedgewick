40bb8f9483d2ff2053c244841ba6c468a2abb267
Ross Marshall <rossmarshallgithub@gmail.com>
2016-04-23 22:53:52 -0400

    Added link to parent to node

--- Chapter04/VisitOrder/node.py
+++ Chapter04/VisitOrder/node.py
@@ -3,6 +3,9 @@
 class Node:
 
     def __init__( self ):
+        self.id = 0
+        self.parent_id = 0
         self.value = "" 
+        self.position = ""   # Relative to parent: L - Left, R - Right
         self.left  = None
         self.right = None
